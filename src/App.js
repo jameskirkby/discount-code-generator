34bee959ebf1b225822c3f7b07621204bfb9df1e
James <james@breaksagency.com>
2017-06-14 13:22:34 +0100

    small chamnge to eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import DatePicker from 'react-datepicker';
-// eslint-disable-next-line
 import moment from 'moment';
 
 import ClipboardButton from 'react-clipboard.js';
